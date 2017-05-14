3394095b2bff9efd68f4c341564cd8e3b2a78722
Gallay Lajos <gallay.lajos@live.com>
2017-05-14 18:16:41 +0200

    CollectionResult values to Partial, Version Update

--- src/api/http-models/CollectionResult.ts
+++ src/api/http-models/CollectionResult.ts
@@ -2,7 +2,7 @@ export class CollectionResult<T> {
     public '@odata.context'?: string;
     public '@odata.nextlink'?: string;
     public '@odata.count'?: number;
-    constructor(public value: T[], count?: number, context?: string, nextlink?: string) {
+    constructor(public value: Array<Partial<T>>, count?: number, context?: string, nextlink?: string) {
         this['@odata.count'] = count;
         this['@odata.context'] = context;
         this['@odata.nextlink'] = nextlink;
