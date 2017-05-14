6dac640d287c9a0d277aedf21b4639236e9543f4
Gallay Lajos <gallay.lajos@live.com>
2017-05-14 12:48:12 +0200

    Add: Endpoint tests for EntityType and EntitySet

--- test/endpointentityset.test.ts
+++ test/endpointentityset.test.ts
@@ -1,12 +1,23 @@
 import * as chai from 'chai';
 import { suite, test } from 'mocha-typescript';
 import { EndpointBuilder } from '../src/endpoint';
+import { PrimaryKey } from '../src/index';
+
+class TestClass {
+    @PrimaryKey
+    public id: number;
+    public val: string;
+}
 
 @suite
 export class EndpointEntitySetTests {
     @test('EndPoint should be constructed')
     public ConstructTest() {
         const endPoint = new EndpointBuilder('api');
+        endPoint.EntityType(TestClass);
+
+        const set = endPoint.EntitySet(TestClass, 'EntitySet');
+        chai.expect(set.Name).to.be.eq('EntitySet');
     }
 
 }
--- test/endpointentitytype.test.ts
+++ test/endpointentitytype.test.ts
@@ -1,12 +1,22 @@
 import * as chai from 'chai';
 import { suite, test } from 'mocha-typescript';
 import { EndpointBuilder } from '../src/endpoint';
+import { EndpointEntityType, ModelDescriptor, ModelDescriptorStore, PrimaryKey } from '../src/index';
+
+class TestClass {
+    @PrimaryKey
+    public id: number;
+    public val: string;
+}
 
 @suite
 export class EndpointEntityTypeTests {
     @test('EndPoint should be constructed')
     public ConstructTest() {
         const endPoint = new EndpointBuilder('api');
+        const type = endPoint.EntityType(TestClass);
+
+        chai.expect(type.Name).to.be.eq(ModelDescriptorStore.GetName(TestClass));
     }
 
 }
